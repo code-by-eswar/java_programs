14c367b01c0da3067e61e2f5154fc55424f8df99
code-by-eswar <eswarchinnamself@gmail.com>
2026-07-04 16:39:27 +0530

    second largest element

---
+++ SecondLargest.java
@@ -0,0 +1,37 @@
+
+import java.util.*;public class SecondLargest {
+  
+
+	public static void main(String[] args) {
+		
+		
+    //by  used sorting
+    /*int arr[] = {10, 324, 45, 90, 9808};
+              Arrays.sort(arr);
+              int seclar = arr[arr.length-2];
+              System.out.println(seclar);
+              */
+    //by used for Loop 
+    int arr[] = {10, 324, 45, 90, 9808};
+    int lar = Integer.MIN_VALUE;
+    int sec =Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++)
+    {
+        if(arr[i]>lar)
+        {
+            sec = lar;
+            lar =arr[i];
+        }
+        else if(arr[i]>sec && arr[i]!=lar)
+        {
+            sec = arr[i];
+        }
+    }
+    System.out.println(sec);
+    
+    
+    
+	}
+}
+
+
