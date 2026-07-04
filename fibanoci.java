d2a1c1bc4599ee7f90c2832d27132a134322c086
code-by-eswar <eswarchinnamself@gmail.com>
2026-07-04 17:08:49 +0530

    git

---
+++ fibanoci.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+
+public class fibanoci
+{
+    public void  fibanociseries(int n)
+    {
+        int first = 0;
+        int second = 1;
+        for(int i=0;i<=n;i++)
+        {
+            
+        System.out.println(first);
+        int next = first+second;
+        first = second;
+        second = next;
+        
+        }
+        
+    }
+	public static void main(String[] args) {
+		
+		
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms");
+        int n = sc.nextInt();
+        fibanoci f = new fibanoci();
+        f.fibanociseries(n);
+	}}
